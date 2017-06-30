3d2a4f5c35252cad3ceb13ea984c02f54abe82f8
Isabella Abatgis <abatgis@gmail.com>
2017-06-30 14:17:21 -0700

    add styling and flex box to results and background

--- js/search-interface.js
+++ js/search-interface.js
@@ -6,6 +6,9 @@ console.log('hello');
 $(document).ready(function() {
   $('#form').submit(function(event) {
     event.preventDefault();
+    var city = $('#city').val();
+    var state = $('#state').val();
+    
     var medicalIssue = $('#issue').val();
     var newSearch = new Doctor();
     newSearch.getDoctors(medicalIssue, apiKey);
--- js/search.js
+++ js/search.js
@@ -2,11 +2,21 @@ function Doctor() {
 
 }
 
+// var apiKey2 = "AIzaSyD4tKeGzcMbXA6RVXoMleFX8KeAcvLI0SI";
+
+// Doctor.prototype.getLocation = function(city, state) {
+//   $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + state + '&key=' + apiKey2).then(function(object) {
+//     var lat = object.results[0].geometry.location.lat;
+//     var lon = object.results[0].geometry.location.lng;
+//     console.log(lat, lon);
+//   }); //close get
+// };
+
 Doctor.prototype.getDoctors = function(medicalIssue, apiKey) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
      for(i = 0; i < result.data.length ; i++)
-     $('#results').append('<div class="doc">' + '<img class="doc-pic" src="' + result.data[i].profile.image_url + '">' + '</br>' +  '<h3>' + result.data[i].profile.first_name + " " +  result.data[i].profile.last_name + " " + result.data[i].profile.title + '</h3>' + '<div class="bio">' + "bio: " + result.data[i].profile.bio + '</div>' + '</div>');
+     $('#results').append('<div class="doc">' + '<div class="doc-title">' + '<img class="doc-pic" src="' + result.data[i].profile.image_url + '">' + '<h3>' + result.data[i].profile.first_name + " " +  result.data[i].profile.last_name + " " + result.data[i].profile.title + '</h3>' + '</div>' + '<div class="bio">' + "bio: " + result.data[i].profile.bio + '</div>' + '</div>');
      console.log(result);
     })
    .fail(function(error){
