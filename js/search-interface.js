01fdddbf40e0ec43bed0af675c26b6051adb786e
Isabella Abatgis <abatgis@gmail.com>
2017-06-30 14:29:16 -0700

    add styling to results

--- js/search-interface.js
+++ js/search-interface.js
@@ -6,9 +6,10 @@ console.log('hello');
 $(document).ready(function() {
   $('#form').submit(function(event) {
     event.preventDefault();
+    $('#results').empty();
     var city = $('#city').val();
     var state = $('#state').val();
-    
+
     var medicalIssue = $('#issue').val();
     var newSearch = new Doctor();
     newSearch.getDoctors(medicalIssue, apiKey);
--- js/search.js
+++ js/search.js
@@ -16,7 +16,7 @@ Doctor.prototype.getDoctors = function(medicalIssue, apiKey) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
      for(i = 0; i < result.data.length ; i++)
-     $('#results').append('<div class="doc">' + '<div class="doc-title">' + '<img class="doc-pic" src="' + result.data[i].profile.image_url + '">' + '<h3>' + result.data[i].profile.first_name + " " +  result.data[i].profile.last_name + " " + result.data[i].profile.title + '</h3>' + '</div>' + '<div class="bio">' + "bio: " + result.data[i].profile.bio + '</div>' + '</div>');
+     $('#results').append('<div class="doc">' + '<div class="doc-title">' + '<img class="doc-pic" src="' + result.data[i].profile.image_url + '">' + '<h3>' + result.data[i].profile.first_name + " " +  result.data[i].profile.last_name + " " + result.data[i].profile.title + '</h3>' + '</div>' + '<div class="bio">' + result.data[i].profile.bio + '</div>' + '</div>');
      console.log(result);
     })
    .fail(function(error){
