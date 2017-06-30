fcc06e470c8301254b29b2a0100db8015027c1ef
Isabella Abatgis <abatgis@gmail.com>
2017-06-30 15:34:53 -0700

    add readme and gh-pages

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,8 +1,6 @@
 var Doctor = require('./../js/search.js').doctorModule;
 var apiKey = require('./../.env').apiKey;
 
-console.log('hello');
-
 $(document).ready(function() {
   $('#form').submit(function(event) {
     event.preventDefault();
